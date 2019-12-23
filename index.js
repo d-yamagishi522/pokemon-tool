281b258a93c57af17ce1176924046be7e86618a5
Your Name <you@example.com>
2019-12-23 18:23:49 +0900

    index.js修正

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const http = require('http')
-const port = process.env.PORT || 8000
-http
-  .createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    response.end('Hello World')
+/* eslint no-console: 0 */
+const { Nuxt, Builder } = require('nuxt')
+const app = require('express')()
+
+// Nuxt.js をインスタンス化
+const config = require('./nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
+config.port = config.dev ? 3000 : process.env.PORT
+const nuxt = new Nuxt(config)
+
+// プロダクション環境ではビルドしない
+const promise = config.dev ? new Builder(nuxt).build() : Promise.resolve()
+promise
+  .then(() => {
+    app.use(nuxt.render)
+    // devだったら3000 productionならprocess.env.PORT
+    console.log('process.env.PORT' + config.port)
+    app.listen(config.port)
+    console.log('Server is listening on http://localhost:3000')
   })
-  .listen(port)
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
